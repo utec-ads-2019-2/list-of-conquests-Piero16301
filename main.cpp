8a0a7c7883336a39172833ccbfd48bf2a32c019c
Piero Angelo Morales Alcalde <38793411+Piero16301@users.noreply.github.com>
2019-08-22 23:12:21 -0500

    Final version UVA

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+string extraerPais(string str) { 
+    istringstream ss(str);
+    string word;
+    ss >> word;
+    return word;
+
+} 
+
+int main () {
+    int iteraciones;
+    string entrada;
+    vector <string> paises;
+    map <string, int> conteoPaises;
+    cin >> iteraciones;
+    for (int i = 0; i <= iteraciones; i++) {
+        string pais_temporal;
+        getline(cin, entrada);
+        pais_temporal = extraerPais(entrada);
+        if (count(paises.begin(), paises.end(), pais_temporal)){
+            conteoPaises[pais_temporal]++;
+        } else {
+            paises.push_back(pais_temporal);
+            conteoPaises[pais_temporal]++;
+        }
+    }
+    map <string, int> :: iterator itr;
+    int count = 0;
+    for (itr = conteoPaises.begin(); itr != conteoPaises.end(); itr++) {
+        if (count != 0) {
+            cout << itr->first << ' ' << itr->second << '\n';
+        }
+        count++;
+    }
     return 0;
 }
